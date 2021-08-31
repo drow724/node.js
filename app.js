0cea2c16cbc6648abca91784b37bdf8da7a1a334
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-08-31 23:00:10 +0900

    POST 요청 처리

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
 //비동기 실행
 app.listen(3000, function() {
     console.log("start!! express server on port 3000");
 });
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 //URL Routing
 app.get('/', function(request, response){
@@ -15,3 +19,9 @@ app.get('/', function(request, response){
 app.get('/main', function(request, response){
     response.sendFile(__dirname + "/public/main.html");
 });
+
+app.post('/email_post', function(request, response){
+    //get : req.param('email');
+    console.log(request.body.email);
+    response.send("<h1>welcome!" + request.body.email + " </h1>");
+})
