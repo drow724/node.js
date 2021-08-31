298c221974fbc346d9c542dcadf1149c0703dade
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-08-31 22:19:21 +0900

    static 디렉토리 설정

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ app.listen(3000, function() {
     console.log("start!! express server on port 3000");
 });
 
+app.use(express.static('public'));
+
+//URL Routing
 app.get('/', function(request, response){
+    response.sendFile(__dirname + "/public/main.html");
+});
+
+app.get('/main', function(request, response){
     response.sendFile(__dirname + "/public/main.html");
 });
