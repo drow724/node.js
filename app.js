cf210558b2ee937a741a341007440bcb2f33f3de
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-09-06 20:48:55 +0900

    Routing 모듈화 2

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-var mysql = require("mysql");
 var cors = require("cors");
 var main = require("./router/main");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3308,
-  user: "root",
-  password: "password",
-  database: "wolfgang",
-});
-
-connection.connect();
+var email = require("./router/email");
 
 //비동기 실행
 app.listen(3000, function () {
@@ -27,39 +17,9 @@ app.use(cors());
 app.set("view engine", "ejs");
 
 app.use("/main", main);
+app.use("/email", email);
 
 //URL Routing
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/public/main.html");
-});
-
-app.get("/main", function (request, response) {
-  response.sendFile(__dirname + "/public/main.html");
-});
-
-app.post("/email_post", function (request, response) {
-  //get : req.param('email');
-  console.log(request.body.email);
-  //response.send("<h1>welcome!" + request.body.email + " </h1>");
-  response.render("email.ejs", '{"email" : request.body.email}');
-});
-
-app.post("/ajax_send_email", function (request, response) {
-  var email = request.body.email;
-  var responseData = {};
-
-  var query = connection.query(
-    'select name from user where email="' + email + '"',
-    function (err, rows) {
-      if (err) throw error;
-      if (rows[0]) {
-        responseData.result = "ok";
-        responseData.name = rows[0].name;
-      } else {
-        responseData.result = "none";
-        responseData.name = "";
-      }
-      response.json(responseData);
-    }
-  );
 });
