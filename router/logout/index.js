4ff8f32e2e887c738142b42003f3c1ee2d0419a9
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-09-07 17:01:50 +0900

    로그아웃 처리

--- router/logout/index.js
+++ router/logout/index.js
@@ -1,12 +1,10 @@
 var express = require("express");
 var app = express();
 var router = express.Router();
-var path = require("path");
 
 router.get("/", function (req, res) {
-  var id = req.user;
-  if (!id) res.render("login.ejs");
-  res.render("main.ejs", { id: id });
+  req.logout();
+  res.redirect("/login");
 });
 
 module.exports = router;
